afef02e2960c6e93bc741822a45237659976c268
darshan tank <tankdarshan01@gmail.com>
2021-01-25 17:31:04 +0530

    make change in event app

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
 from users import views as views1
+from . import views
 
 urlpatterns = [
     path('', views.home, name='event-home'),
@@ -8,8 +8,16 @@ urlpatterns = [
     path('about/', views.about, name='event-about'),
     path('register/', views1.register, name='users-register'),
     path('event_view/add/', views.event_add, name='event_add'),
-    path('event_view/getid/', views.get_id, name='get_id'),
-    path('event_view/getid/update', views.event_update, name='event_update'),
-    path('event_view/getid/delete', views.event_delete, name='event_delete'),
-    path('event_details/<int:my_id>', views.event_details, name='event_details')
+
+    path('event_view/<int:my_id>/update',
+         views.event_update, name='event_update'),
+    path('event_view/<int:my_id>/updated',
+         views.event_updated, name='event_updated'),
+
+    path('event_view/<int:my_id>/delete',
+         views.event_delete, name='event_delete'),
+    path('event_details/<int:my_id>', views.event_details, name='event_details'),
+    path('user_Details/<slug:username>',
+         views.user_details, name='user_details'),
+
 ]
